9d41fc1672a1ec451c5ece51583ba9290a10f284
Neras <medianeras57@gmail.com>
2018-07-09 22:30:48 +0800

    add string pointer

--- pointer/pointer.go
+++ pointer/pointer.go
@@ -8,4 +8,15 @@ func main() {
 	var intP *int
 	intP = &i1
 	fmt.Printf("The value at memory location %p is %d\n", intP, *intP)
+	stringPointer()
+}
+
+func stringPointer() {
+	s := "good bye"
+	var p *string = &s
+	*p = "ciao"
+	fmt.Printf("Here is the pointer p: %p\n", p)
+	fmt.Printf("Here is the string *p: %s\n", *p)
+	fmt.Printf("Here is the string s: %s\n", s)
+	fmt.Printf("Here is the string %v\n", &s)
 }
