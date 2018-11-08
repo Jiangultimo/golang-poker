c324da1a42702f912d069033501602b9ec680f9b
Neras <medianeras57@gmail.com>
2018-11-08 14:12:25 +0800

    add pointer code & crash code

--- pointer/pointer.go
+++ pointer/pointer.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func main() {
 	var i1 = 5
-	fmt.Printf("An integer: %d, its location in memory: %p\n", i1, &i1)
+	fmt.Printf("An integer: %d, its location in memory: %p\n", i1, &i1) // 取地址符 &
 	var intP *int
 	intP = &i1
 	fmt.Printf("The value at memory location %p is %d\n", intP, *intP)
--- pointer/testcrash.go
+++ pointer/testcrash.go
@@ -1,1 +1,8 @@
 package main
+
+func main() {
+	var p *int = nil
+	*p = 0
+}
+// in windows: stops only with: <exit code="-1073741819" msg="process crashed" />
+// runtime error: invalid memory address or nil pointer dereference
