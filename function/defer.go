35a3d079c39e9323ea8845798bffe19bb435eed8
Neras <medianeras57@gmail.com>
2018-11-10 23:41:41 +0800

    add defer description

--- function/defer.go
+++ function/defer.go
@@ -2,6 +2,7 @@ package main
 
 import "fmt"
 
+// 关键字defer允许我们推迟到函数返回之前（或任意位置执行return语句之后）一刻才执行某个语句或函数
 func function1() {
 	fmt.Print("In function1 at the top\n")
 	defer function2()
@@ -15,5 +16,10 @@ func function2() {
 func main() {
 	function1()
 }
+
+// print
+// In function1 at the top
+// In function1 at the botton
+// Function2: Deferred until the end of the calling function!
 
 
