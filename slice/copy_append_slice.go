51e2f1218497c0bd0963a2dd98263e8b2632f567
Neras <medianeras57@gmail.com>
2018-11-11 23:43:55 +0800

    add slice operation

--- slice/copy_append_slice.go
+++ slice/copy_append_slice.go
@@ -1,1 +1,29 @@
 package main
+
+import "fmt"
+
+func main() {
+	sl_from := []int{1, 2, 3}
+	sl_to := make([]int, 10)
+
+	n := copy(sl_to, sl_from)
+	fmt.Println(sl_to)
+	fmt.Printf("Copied %d elements\n", n)
+
+	sl3 := []int{1, 2, 3}
+	sl3 = append(sl3, 4, 5, 6)
+	fmt.Println(sl3)
+}
+
+func AppendByte(slice []byte, data ...byte) []byte {
+	m := len(slice)
+	n := m + len(data)
+	if n > cap(slice) {
+		newSlice := make([]byte, (n+1) * 2)
+		copy(newSlice, slice)
+		slice = newSlice
+	}
+	slice = slice[0:n]
+	copy(slice[m:n], data)
+	return slice
+}
--- slice/slices_forrange2.go
+++ slice/slices_forrange2.go
@@ -5,7 +5,7 @@ import "fmt"
 func main() {
 	seasons := []string{"Spring", "Sumber", "Autum", "Winter"}
 	for ix, season := range seasons {
-		fmt.Printf("Season %d is $s\n", ix, season)
+		fmt.Printf("Season %d is %s\n", ix, season)
 	}
 	var season string
 	for _, season = range seasons {
