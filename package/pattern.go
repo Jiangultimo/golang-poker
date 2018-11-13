a4100572a4f37efa3f0fae433e07b9ba81f1cbfa
Neras <medianeras57@gmail.com>
2018-11-13 21:46:15 +0800

    add package

---
+++ package/pattern.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"regexp"
+	"strconv"
+)
+
+func main() {
+	// 目标字符串
+	searchIn := "John: 2578.34 William: 4567.23 Steve: 5632.18"
+	pat := "[0-9]+.[0-9]+" // 正则
+	f := func(s string) string {
+		v, _ := strconv.ParseFloat(s, 32)
+		return strconv.FormatFloat(v * 2, 'f', 2, 32)
+	}
+
+	if ok, _ := regexp.Match(pat, []byte(searchIn)); ok {
+		fmt.Println("Match Found!")
+	}
+
+	re, _ := regexp.Compile(pat)
+	str := re.ReplaceAllString(searchIn, "##.#")
+	fmt.Println(str)
+	str2 := re.ReplaceAllStringFunc(searchIn, f)
+	fmt.Println(str2)
+}
