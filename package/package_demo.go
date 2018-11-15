2ae05eacc659cc2630659b41240ece5d639279ff
Neras <medianeras57@gmail.com>
2018-11-15 23:39:37 +0800

    supplement struct

---
+++ package/package_demo.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"./pack1/pack1"
+)
+
+func main() {
+	var test1 string
+	test1 = pack1.ReturnStr()
+	fmt.Printf("ReturnStr from package1: %s\n", test1)
+	fmt.Printf("Integer from package1: %d\n", pack1.Pack1Int)
+}
