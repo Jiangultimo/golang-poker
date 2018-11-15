e795f45764eb891a178fa1b0227212216eb07535
Neras <medianeras57@gmail.com>
2018-11-15 23:40:02 +0800

    supplement method

--- method/method_on_time.go
+++ method/method_on_time.go
@@ -1,1 +1,20 @@
 package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type myTime struct {
+	time.Time
+}
+
+func (t myTime) first3Chars() string {
+	return t.Time.String()[0:3]
+}
+
+func main() {
+	m := myTime{time.Now()}
+	fmt.Println("Full time now:", m.String())
+	fmt.Println("First 3 chars:", m.first3Chars())
+}
