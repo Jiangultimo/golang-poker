15d90df39fce1fbc3720164e45ebbad07cd72ae0
Neras <medianeras57@gmail.com>
2018-11-16 22:18:33 +0800

    add interface pratice

--- interface/interfaces.go
+++ interface/interfaces.go
@@ -1,1 +1,24 @@
-package _interface
+package  main
+
+import "fmt"
+
+type Shaper interface {
+	Area() float32
+}
+
+type Square struct {
+	side float32
+}
+
+func (sq *Square) Area() float32 {
+	return sq.side * sq.side
+}
+
+func main() {
+	sq1 := &Square{5} // 等价于 sq1 := new(Square); sq1.side = 5
+
+	var areaIntf Shaper
+	areaIntf = sq1
+
+	fmt.Printf("The square has area: %f\n", areaIntf.Area())
+}
--- interface/interfaces_poly.go
+++ interface/interfaces_poly.go
@@ -1,1 +1,36 @@
 package main
+
+import "fmt"
+
+type Shaper interface {
+	Area() float32
+}
+
+type Square struct {
+	side float32
+}
+
+func (sq *Square) Area() float32 {
+	return sq.side * sq.side
+}
+
+type Rectangle struct {
+	length float32
+	width float32
+}
+
+func (r Rectangle) Area() float32 {
+	return r.length * r.width
+}
+
+func main() {
+	r := Rectangle{5, 3}
+	q := &Square{5}
+
+	shapes := []Shaper{r, q}
+	fmt.Println("Looping through shapes for area ...")
+	for n, _ := range shapes {
+		fmt.Println("Shape details: ", shapes[n])
+		fmt.Println("Area of this shape is: ", shapes[n].Area())
+	}
+}
