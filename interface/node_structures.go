18f0269c77be33e5d12eabd24015b86bd784d458
Neras <medianeras57@gmail.com>
2018-11-18 23:20:58 +0800

    add empty interface 2

---
+++ interface/node_structures.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+type Node struct {
+	le *Node
+	data interface{}
+	ri *Node
+}
+
+func NewNode(left, right *Node) *Node {
+	return &Node{left, nil, right}
+}
+
+func (n *Node) SetData(data interface{}) {
+	n.data = data
+}
+
+func main() {
+	root := NewNode(nil, nil)
+	root.SetData("root node")
+	// make child (leaf) nodes:
+	a := NewNode(nil, nil)
+	a.SetData("left node")
+	b := NewNode(nil, nil)
+	b.SetData("right node")
+	root.le = a
+	root.ri = b
+	fmt.Printf("%v\n", root)
+}
