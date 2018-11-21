6c215ec7323608d173ff3a847845076e3c22ce98
Neras <medianeras57@gmail.com>
2018-11-21 22:02:44 +0800

    add read file by useing slice

--- professional/RWData/cat2.go
+++ professional/RWData/cat2.go
@@ -1,5 +1,41 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+func cat(f *os.File) {
+	const NBUF = 512
+	var buf [NBUF]byte
+
+	for {
+		switch nr, err := f.Read(buf[:]); true {
+		case nr < 0:
+			fmt.Fprintf(os.Stderr, "cat: error reading: %s\n", err.Error())
+		case nr == 0: // EOF
+			return
+		case nr > 0:
+			if nw, ew := os.Stdout.Write(buf[0:nr]);nw != nr {
+				fmt.Fprintf(os.Stderr, "cat: error writing: %s\n", ew.Error())
+			}
+		}
+	}
+}
+
 func main() {
-	$END$
+	flag.Parse() // scans the arg list and sets up flags
+	if flag.NArg() == 0 { // 如果没有参数，直接打印命令行输入的内容
+		cat(os.Stdin)
+	}
+	for i := 0; i < flag.NArg(); i++ {
+		f, err := os.Open(flag.Arg(i))
+		if f == nil {
+			fmt.Fprintf(os.Stderr, "cat: can't open %s: error %s\n", flag.Arg(i), err)
+			os.Exit(1)
+		}
+		cat(f)
+		f.Close()
+	}
 }
