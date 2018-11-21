4aa492507475b0f8fa8a5a46170c049cc37d7871
Neras <medianeras57@gmail.com>
2018-11-21 23:37:22 +0800

    go Cryptography

---
+++ professional/RWData/hash_sha1.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"crypto/sha1"
+	"fmt"
+	"io"
+	"log"
+)
+
+func main() {
+	hasher := sha1.New()
+	io.WriteString(hasher, "test")
+	b := []byte{}
+	fmt.Printf("Result: %x\n", hasher.Sum(b))
+	fmt.Printf("Result: %d\n", hasher.Sum(b))
+
+	hasher.Reset()
+	data := []byte("We shall overcome!")
+	n, err := hasher.Write(data)
+	if n != len(data) || err != nil {
+		log.Printf("Hash write error: %v / %v", n, err)
+	}
+	checksum := hasher.Sum(b)
+	fmt.Printf("Result: %x\n", checksum)
+}
