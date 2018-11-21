3cceec7f84537a62c26b14142d0cc5cdc97b63e0
Neras <medianeras57@gmail.com>
2018-11-21 22:13:52 +0800

    add the example of useing interfaces code

--- professional/RWData/io_interfaces.go
+++ professional/RWData/io_interfaces.go
@@ -1,1 +1,22 @@
 package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	// unbuffered
+	a, err := fmt.Fprintf(os.Stdout, "%s\n", "hello world! - unbuffered")
+	if err != nil {
+		fmt.Printf("%s\n", err)
+	}
+	fmt.Println(a)
+	fmt.Printf("the len of 'hello world! - unbuffered' is : %d\n", len("hello world! - unbuffered"))
+	// buffered: os.Stdout implements io.Writer
+	buf := bufio.NewWriter(os.Stdout)
+	// and now so does buf
+	fmt.Fprintf(buf, "%s\n", "hello world! - buffered")
+	buf.Flush()
+}
