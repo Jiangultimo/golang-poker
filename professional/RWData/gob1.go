8cca8649bcf4a9d4371c22a2e7d67164a0349eb5
Neras <medianeras57@gmail.com>
2018-11-21 23:29:35 +0800

    add gob code

--- professional/RWData/gob1.go
+++ professional/RWData/gob1.go
@@ -1,1 +1,38 @@
 package main
+
+import (
+	"bytes"
+	"encoding/gob"
+	"fmt"
+	"log"
+)
+
+type P struct {
+	X, Y, Z int
+	Name string
+}
+
+type Q struct {
+	X, Y *int32
+	Name string
+}
+
+func main() {
+	// 初始化encoder和decoder
+	// Normally enc and dec would be bound to network connections and the encoder and decoder would run in defferent processes
+	var network bytes.Buffer // Stand-in for a network connection
+	enc := gob.NewEncoder(&network) // will write to network
+	dec := gob.NewDecoder(&network) // will read from network
+	// Encode (send) the value.
+	err := enc.Encode(P{3, 4, 5, "Pythagoras"})
+	if err != nil {
+		log.Fatal("encode error:", err)
+	}
+	// Decode (receive) the value.
+	var q Q
+	err = dec.Decode(&q)
+	if err != nil {
+		log.Fatal("decode error:", err)
+	}
+	fmt.Printf("%q: {%d, %d}\n", q.Name, *q.X, *q.Y)
+}
--- professional/RWData/gob2.go
+++ professional/RWData/gob2.go
@@ -1,1 +1,35 @@
 package main
+
+import (
+	"encoding/gob"
+	"log"
+	"os"
+)
+
+type Address struct {
+	Type string
+	City string
+	Country string
+}
+
+type VCard struct {
+	FirstName string
+	LastName string
+	Address []*Address
+	Remark string
+}
+
+var content string
+
+func main() {
+	pa := &Address{"private", "Aartselaar", "Belgium"}
+	wa := &Address{"wrok", "Boom", "Belgium"}
+	vc := VCard{"Jan", "kersschot", []*Address{pa, wa}, "none"}
+	file, _ := os.OpenFile("vcard.gob", os.O_CREATE|os.O_WRONLY, 0666)
+	defer file.Close()
+	enc := gob.NewEncoder(file)
+	err := enc.Encode(vc)
+	if err != nil {
+		log.Println("Error in encoding gob")
+	}
+}
