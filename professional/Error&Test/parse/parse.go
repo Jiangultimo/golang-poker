f1cf7abc25a328dbdcbd671df5af2a10b303bbf8
Neras <medianeras57@gmail.com>
2018-11-22 22:46:09 +0800

    add panic test

--- professional/Error&Test/parse/parse.go
+++ professional/Error&Test/parse/parse.go
@@ -1,1 +1,47 @@
 package parse
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+type ParseError struct {
+	Index int
+	Word string
+	Err error
+}
+
+func (e *ParseError) String() string {
+	return fmt.Sprintf("pkg parse: error parsing %q as int", e.Word)
+}
+
+func Parse(input string) (numbers []int, err error) {
+	defer func() {
+		if r := recover(); r != nil {
+			var ok bool
+			err, ok = r.(error)
+			if !ok {
+				err = fmt.Errorf("pfk: %v", r)
+			}
+		}
+	}()
+
+	fields := strings.Fields(input)
+	numbers = fields2numbers(fields)
+	return
+}
+
+func fields2numbers(fields []string) (numbers []int) {
+	if len(fields) == 0 {
+		panic("no words to parse")
+	}
+	for idx, field := range fields {
+		num, err := strconv.Atoi(field)
+		if err != nil {
+			panic(&ParseError{idx, field, err})
+		}
+		numbers = append(numbers, num)
+	}
+	return
+}
