8268c6cfd5887c4d51e6d93c27e70e511a3aac23
Neras <medianeras57@gmail.com>
2018-11-22 22:05:47 +0800

    add handle error

--- professional/Error&Test/error.go
+++ professional/Error&Test/error.go
@@ -1,1 +1,12 @@
-package Error_Test
+package main
+
+import (
+	"fmt"
+	"github.com/pkg/errors"
+)
+
+var errNotFound error = errors.New("Not Found error")
+
+func main() {
+	fmt.Printf("error: %v", errNotFound)
+}
