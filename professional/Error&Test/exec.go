c59573d0e22c6a7b15263074f84747f1a0652223
Neras <medianeras57@gmail.com>
2018-11-25 21:50:22 +0800

    add exec code

--- professional/Error&Test/exec.go
+++ professional/Error&Test/exec.go
@@ -1,1 +1,25 @@
 package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	// 1. os.StartProcess
+	env := os.Environ()
+	procAttr := &os.ProcAttr{
+		Env: env,
+		Files: []*os.File{
+			os.Stdin,
+			os.Stdout,
+			os.Stderr,
+		},
+	}
+	pid, err := os.StartProcess("/bin/ls", []string{"ls", "-l"}, procAttr)
+	if err != nil {
+		fmt.Printf("Error %v starting process!", err)
+		os.Exit(1)
+	}
+	fmt.Printf("The process id is %v", pid)
+}
