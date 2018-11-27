460c8b8c3401c860bd3feb666dc3b958f9ae8e02
Neras <medianeras57@gmail.com>
2018-11-27 23:43:42 +0800

    update gopl

---
+++ gopl.io/1.5/fetch.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
+func main() {
+	for _, url := range os.Args[1:] { // 获取命令行输入的url
+		resp, err := http.Get(url) // 请求url
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+		b, err := ioutil.ReadAll(resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url ,err)
+			os.Exit(1)
+		}
+		fmt.Printf("%s", b)
+	}
+}
