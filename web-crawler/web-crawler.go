2bbda76e1061794896bf2a09200a4de6b50d7284
Neras <medianeras57@gmail.com>
2018-12-02 22:07:56 +0800

    add web crawler exercise

---
+++ web-crawler/web-crawler.go
@@ -0,0 +1,74 @@
+package main
+
+import "fmt"
+
+type Fetcher interface {
+	// Fetch返回 URL 的 body 内容，并且将在这个页面上找到 URL 放到一个 slice 中
+	Fetch(url string) (body string, urls []string, err error)
+}
+
+func Crawl(url string, depth int, fetcher Fetcher) {
+	if depth <= 0 {
+		return
+	}
+	body, urls, err := fetcher.Fetch(url)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("Found: %s %q\n", url ,body)
+	for _, u := range urls {
+		Crawl(u, depth - 1, fetcher)
+	}
+}
+
+func main() {
+	Crawl("https://golang.org/", 4, fetcher)
+}
+
+type fakeFetcher map[string]*fakeResult
+
+type fakeResult struct {
+	body string
+	urls []string
+}
+
+func (f fakeFetcher) Fetch(url string) (string, []string, error) {
+	if res, ok := f[url]; ok {
+		return res.body, res.urls, nil
+	}
+	return "", nil, fmt.Errorf("not found: %s", url)
+}
+
+var fetcher = fakeFetcher{
+	"https://golang.org/": &fakeResult{
+		"The Go Programming Language",
+		[]string{
+			"https://golang.org/pkg/",
+			"https://golang.org/cmd/",
+		},
+	},
+	"https://golang.org/pkg/": &fakeResult{
+		"Packages",
+		[]string{
+			"https://golang.org/",
+			"https://golang.org/cmd/",
+			"https://golang.org/pkg/fmt/",
+			"https://golang.org/pkg/os/",
+		},
+	},
+	"https://golang.org/pkg/fmt/": &fakeResult{
+		"Package fmt",
+		[]string{
+			"https://golang.org/",
+			"https://golang.org/pkg/",
+		},
+	},
+	"https://golang.org/pkg/os/": &fakeResult{
+		"Package os",
+		[]string{
+			"https://golang.org/",
+			"https://golang.org/pkg/",
+		},
+	},
+}
