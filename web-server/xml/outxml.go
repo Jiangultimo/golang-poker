5a7d1ca70207e974bf0d6fbe006c29cc35a18344
Neras <medianeras57@gmail.com>
2018-12-17 21:16:08 +0800

    add xml

---
+++ web-server/xml/outxml.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"encoding/xml"
+	"fmt"
+	"os"
+)
+
+type Servers struct {
+	XMLName xml.Name `xml:"servers"`
+	Version string `xml:"version.attr"`
+	Svs []server `xml:"server"`
+}
+
+type server struct {
+	ServerName string `xml: "serverName"`
+	ServerIP string `xml:"serverIP"`
+}
+
+func main() {
+	v := &Servers{Version: "1"}
+	v.Svs = append(v.Svs, server{"Shanghai_VPN", "127.0.0.1"})
+	v.Svs = append(v.Svs, server{"Beijing_VPN", "127.0.0.2"})
+	output, err := xml.MarshalIndent(v, " ", "  ")
+	if err != nil {
+		fmt.Printf("error: %v\n", err)
+	}
+	os.Stdout.Write([]byte(xml.Header)) // 是输出带xml头
+	os.Stdout.Write(output)
+}
