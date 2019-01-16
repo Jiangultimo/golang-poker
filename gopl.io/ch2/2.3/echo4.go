c21af29165739f456d4c049856c513dafc67f3eb
Neras <medianeras57@gmail.com>
2019-01-17 00:03:30 +0800

    update gopl.io

--- gopl.io/ch2/2.3/echo4.go
+++ gopl.io/ch2/2.3/echo4.go
@@ -1,1 +1,21 @@
-package __3
+package main
+
+import (
+	"flag"
+	"fmt"
+	"strings"
+)
+
+var n = flag.Bool("n", false, "omit trailing newline")
+var sep = flag.String("s", " ", "separator")
+
+func main() {
+	flag.Parse()
+	fmt.Println(*n)
+	fmt.Println(*sep)
+	fmt.Println(flag.Args())
+	fmt.Printf(strings.Join(flag.Args(), *sep))
+	if !*n {
+		fmt.Println()
+	}
+}
--- gopl.io/ch2/2.5/tempconv.go
+++ gopl.io/ch2/2.5/tempconv.go
@@ -1,1 +1,33 @@
-package __5
+package main
+
+import "fmt"
+
+type Celsius float64 // 摄氏度
+type Fahrenheit float64 // 华氏温度
+
+const (
+	AbsoluteZeroC Celsius = -273.15 // 绝对零度
+	FreezingC Celsius = 0 // 结冰点温度
+	BoilingC Celsius = 100 // 沸水温度
+)
+
+// 传入摄氏度，返回华氏温度
+// 类型转换，类型转换不会改变值本身，但是会使他们的语义发生变化
+func CtoF(c Celsius) Fahrenheit {
+	return Fahrenheit(c * 9 / 5 + 32)
+}
+
+// 传入华氏温度，返回设置温度
+func FtoC(f Fahrenheit) Celsius {
+	return Celsius((f - 32) * 5 / 9)
+}
+
+func main() {
+	fmt.Printf("%g\n", BoilingC - FreezingC)
+	boilingF := CtoF(BoilingC)
+	fmt.Printf("%g\n", boilingF - CtoF(FreezingC))
+	// 不同类型不能直接进行操错
+	fmt.Printf("%g\n", boilingF - FreezingC) // compile error: type mismatch
+}
+
+
