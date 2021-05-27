159df4378903b924abcdd92577964b0da46c6ffe
Neras <medianeras57@gmail.com>
2021-05-27 23:52:52 +0800

    feature: add Slice

--- gopl.io/ch4/4.2/append.go
+++ gopl.io/ch4/4.2/append.go
@@ -1,1 +1,58 @@
 package main
+
+import "fmt"
+
+func main() {
+    var x, y []int
+    for i := 0; i < 10; i++ {
+        y = appendInt(x, i)
+        fmt.Printf("%d cap=%d\t%v\n", i ,cap(y), y)
+        x = y
+    }
+
+    data := []string{"one", "", "three"}
+    fmt.Printf("%q\n", nonempty(data))
+    fmt.Printf("%q\n", data)
+
+    data2 := []string{"one", "", "three"}
+    fmt.Printf("%q\n", nonempty2(data2))
+    fmt.Printf("%q\n", data2)
+}
+
+func appendInt(x []int, y int) []int {
+    var z []int
+    zlen := len(x) + 1
+    if zlen <= cap(x) {
+        z = x[:zlen]
+    } else {
+        zcap := zlen
+        if zcap < 2*len(x) {
+            zcap = 2*len(x)
+        }
+        z = make([]int, zlen, zcap)
+        copy(z, x)
+    }
+    z[len(x)] = y
+    return z
+}
+
+func nonempty(strings []string) []string {
+    i := 0
+    for _, s := range strings {
+        if s != "" {
+            strings[i] = s
+            i++
+        }
+    }
+    return strings[:i]
+}
+
+func nonempty2(strings []string) []string {
+    out := strings[:0]
+    for _, s := range strings {
+        if s != "" {
+            out = append(out, s)
+        }
+    }
+    return out
+}
